3b9a845de0f15f3e53d0ce8c0dd64e0a8fb11e5b
NKanoya <3397540942@qq.com>
2025-11-17 20:50:34 +0800

    [LibraryManagement] (feat) build book model
    - improve the construction of class `Book` , now composed of `BookInfo` and `CopyEntries`
    - rename some properties

--- 01-LibraryManagement/include/core/book.hpp
+++ 01-LibraryManagement/include/core/book.hpp
@@ -6,12 +6,63 @@
 #define LIBRARYMANAGEMENT_BOOK_HPP
 
 #include <string>
+#include <vector>
+#include <memory>
 
 struct BookInfo {
-    std::string ISPN;
-    std::string name;
+    std::string ISBN;
+    std::string title;
     std::string author;
-    std::string publishingHouse;
+    std::string publisher;
+    std::string year;
+    std::string summary;
+};
+
+enum class CopyStatus {
+    available,
+    borrowed,
+    onLoan
+};
+
+struct CopyInfo {
+    std::string innerNumber;
+
+};
+
+using CopyEntry = std::unique_ptr<CopyInfo>;
+using CopyEntries = std::vector<CopyEntry>;
+
+
+class Book {
+    BookInfo m_info;
+
+    CopyEntries m_copy;
+public:
+    // original Book constructor: construct an empty Book object
+    Book() : m_info({{},{},{},{}, {}, {}}) {}
+
+    Book(BookInfo info, CopyInfo copyInfo);
+    Book(BookInfo info, CopyEntries copyInfoEntries);
+
+    Book(const Book& oth) = delete;
+    Book& operator=(const Book& oth) = delete;
+
+    Book(Book&& oth) noexcept;
+    Book& operator=(Book&& oth) noexcept;
+
+    BookInfo& getInfo() noexcept;
+    const BookInfo& getInfo() const noexcept;
+
+    inline bool valid() const noexcept {
+        return !m_info.ISBN.empty();
+    }
+
+    inline operator bool() const noexcept {
+        return !m_info.ISBN.empty();
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, Book& book) noexcept;
+    friend std::istream& operator>>(std::istream& is, Book& book) noexcept;
 };
 
 #endif //LIBRARYMANAGEMENT_BOOK_HPP
--- 01-LibraryManagement/src/book/book.cpp
+++ 01-LibraryManagement/src/book/book.cpp
@@ -4,3 +4,28 @@
 
 #include "core/book.hpp"
 
+Book::Book(Book &&oth) noexcept : m_info(std::move(oth.m_info)) {}
+
+Book &Book::operator=(Book &&oth) noexcept {
+    if(&oth == this) {
+        return *this;
+    }
+    m_info = std::move(oth.m_info);
+    return *this;
+}
+
+Book::Book(BookInfo bookInfo, CopyInfo copyInfo)
+    : m_info(std::move(bookInfo)) {
+    m_copy.reserve(1);     // reserve space for 1 copy entry
+    m_copy.push_back(std::make_unique<CopyInfo>(std::move(copyInfo)));   // store the entry
+}
+
+Book::Book(BookInfo bookInfo, CopyEntries copyInfoEntries)
+    : m_info(std::move(bookInfo)),
+      m_copy(std::move(copyInfoEntries)) {}
+
+
+
+
+
+
