0b99275c9d0ad8e0709751ebaf8bb8aa3087249a
NKanoya <3397540942@qq.com>
2025-11-18 20:38:56 +0800

    [LibraryManagement] (utils) add CSVReader class and CSV parsing logic
    - standardize the naming of `namespace Utils`
    - introduce a new `Utils::CSVReader` class for reading CSV files, including parsing logic to handle quoted fields and escaped quotation marks.

--- 01-LibraryManagement/include/utils/csv.hpp
+++ 01-LibraryManagement/include/utils/csv.hpp
@@ -8,23 +8,61 @@
 #include <cinttypes>
 #include <vector>
 #include <string>
+#include <iostream>
 
 
-class CSVParser {
-    std::vector<std::string> m_keys;
-    unsigned int m_cols;
-public:
-    explicit CSVParser(std::vector<std::string> m_keys);
+namespace Utils {
 
-    CSVParser(const CSVParser& oth) = delete;
-    CSVParser& operator=(const CSVParser& oth) = delete;
-    CSVParser(CSVParser&& oth) = delete;
-    CSVParser& operator=(CSVParser&& oth) = delete;
+    class CSVParser {
+        std::vector<std::string> m_keys;
+        unsigned int m_cols;
+    public:
+        explicit CSVParser(std::vector<std::string> m_keys);
 
-    void writeHeader(std::ostream& os) const;
-    bool addEntry(std::ostream& os, const std::vector<std::string>& contents) const;
+        CSVParser(const CSVParser& oth) = delete;
+        CSVParser& operator=(const CSVParser& oth) = delete;
+        CSVParser(CSVParser&& oth) = delete;
+        CSVParser& operator=(CSVParser&& oth) = delete;
 
-    inline unsigned int columnCount() const noexcept { return m_cols; }
-};
+        void writeHeader(std::ostream& os) const;
+        bool addEntry(std::ostream& os, const std::vector<std::string>& contents) const;
+
+        inline unsigned int columnCount() const noexcept { return m_cols; }
+    };
+
+    class CSVReader {
+        std::size_t m_cols;
+    public:
+        CSVReader(std::size_t columnCount) : m_cols(columnCount) {}
+        CSVReader(std::istream& is);
+
+        CSVReader(const CSVReader& oth) = delete;
+        CSVReader& operator=(const CSVReader& oth) = delete;
+        CSVReader(CSVReader&& oth) = delete;
+        CSVReader& operator=(CSVReader&& oth) = delete;
+
+        bool readLine(std::istream &is, const std::vector<std::string *> &inputList);
+
+        inline bool valid() const noexcept {
+            return m_cols;
+        }
+
+        inline operator bool() const noexcept {
+            return m_cols;
+        }
+
+    };
+
+    enum class ReadState {
+        inQuotation,                // between quotation marks
+        lookingForComma,            // after the ending quotation mark, looking for next comma
+        lookingForQuotation         // after the comma, looking for next beginning quotation mark
+    };
+
+
+
+
+}
+
 
 #endif //LIBRARYMANAGEMENT_CSV_HPP
--- 01-LibraryManagement/src/csv/csv.cpp
+++ 01-LibraryManagement/src/csv/csv.cpp
@@ -5,64 +5,178 @@
 #include "utils/csv.hpp"
 #include <iostream>
 
-static void escapeQuotationMarks(std::vector<std::string>& list, std::size_t size) {
-    // define the end sign iterator
+namespace Utils {
+    CSVParser::CSVParser(std::vector<std::string> keys)
+            : m_keys(std::move(keys)), m_cols(m_keys.size()) {}
 
-}
-
-CSVParser::CSVParser(std::vector<std::string> keys)
-    : m_keys(std::move(keys)), m_cols(m_keys.size()) {}
-
-void CSVParser::writeHeader(std::ostream& os) const {
-    for(int i = 0; i < m_cols; i++) {
-        os << m_keys[i];
-        if(i != m_cols - 1) {
-            os << ",";
-        }
-    }
-    os << '\n';
-}
-
-static void outputEscapeString(std::ostream &os,
-                               const std::vector<std::string> &contents,
-                               decltype(contents.end()) endIt) {
-    // traverse assigned strings
-    for(auto it = contents.begin(); it != endIt; ++it) {
-        os << '"';                 // beginning quotation mark
-        for(auto& ch: *it) {
-            if(ch == '\"') {
-                os << "\"\"";       // escape `"` as `""`
-            } else {
-                os << ch;           // copy the original string
+    void CSVParser::writeHeader(std::ostream& os) const {
+        for(int i = 0; i < m_cols; i++) {
+            os << m_keys[i];
+            if(i != m_cols - 1) {
+                os << ",";
             }
         }
-
-        os << '"';                 // ending quotation mark
-
-        if (it + 1 != endIt) {
-            os << ',';             // separating comma
-        }
-    }
-}
-
-bool CSVParser::addEntry(std::ostream &os, const std::vector<std::string> &contents) const {
-    auto endIt = contents.end();
-    if(contents.size() >= m_cols) {
-        endIt = contents.begin() + m_cols;
-        outputEscapeString(os, contents, endIt);
-    } else {
-        std::size_t absentColumnNumber = m_cols - contents.size();
-        outputEscapeString(os, contents, endIt);
-        for(int i = 0; i < absentColumnNumber; ++i) {
-            os << ",\"\"";
-        }
-
+        os << '\n';
     }
 
-    os << '\n';
-    return (bool) os;
+    static void outputEscapeString(std::ostream &os,
+                                   const std::vector<std::string> &contents,
+                                   decltype(contents.end()) endIt) {
+        // traverse assigned strings
+        for(auto it = contents.begin(); it != endIt; ++it) {
+            os << '"';                 // beginning quotation mark
+            for(auto& ch: *it) {
+                if(ch == '\"') {
+                    os << "\"\"";       // escape `"` as `""`
+                } else {
+                    os << ch;           // copy the original string
+                }
+            }
+
+            os << '"';                 // ending quotation mark
+
+            if (it + 1 != endIt) {
+                os << ',';             // separating comma
+            }
+        }
+    }
+
+    bool CSVParser::addEntry(std::ostream &os, const std::vector<std::string> &contents) const {
+        auto endIt = contents.end();
+        if(contents.size() >= m_cols) {
+            endIt = contents.begin() + m_cols;
+            outputEscapeString(os, contents, endIt);
+        } else {
+            std::size_t absentColumnNumber = m_cols - contents.size();
+            outputEscapeString(os, contents, endIt);
+            for(int i = 0; i < absentColumnNumber; ++i) {
+                os << ",\"\"";
+            }
+
+        }
+
+        os << '\n';
+        return (bool) os;
+    }
+
+
+    static bool traverse(const std::string& line, std::vector<std::string_view>& strings, std::size_t columnCount) {
+
+        ReadState state = ReadState::lookingForQuotation;    // init state: lookingForQuotation
+
+        // the substring properties (beginChar + length)
+        int substringIndex = 0;
+        const char * beginChar = &line[0];
+        std::size_t length = 0;
+
+        for(auto it = line.begin(); it != line.end(); ++it) {
+            auto ch = *it;
+            switch(ch) {
+                case '\"': {
+                    
+                    switch (state) {
+                        case ReadState::lookingForQuotation: {
+                            state = ReadState::inQuotation;    // update read state -> inQuotation
+                            beginChar = &*(it + 1);       // identify next character as the beginning of a new substring
+                            break;
+                        }
+                        case ReadState::inQuotation: {
+                            // check if it's ending quotation mark or a escaped one
+                            if (*(it + 1) == '\"') {   // parse a `""` inside quotation
+                                it += 1;              // ignore the escaped quotation
+                                length += 2;
+                            } else {    // face ending quotation mark
+                                // end of the substring
+
+                                new(&strings[substringIndex]) std::string_view{beginChar, length};
+                                substringIndex++;
+                                length = 0;         // reinitialize
+
+                                // parse enough substring
+                                if (substringIndex == columnCount) {
+                                    return true;
+                                } else {
+                                    state = ReadState::lookingForComma;     // looking for the next comma
+                                }
+                            }
+                            break;
+                        }
+                        case ReadState::lookingForComma : {
+                            // error: just pass it
+                        }
+                    }
+
+                    break;
+                }
+
+                case ',': {
+                    switch (state) {
+                        case ReadState::lookingForComma: {
+                            // ready for the next quotation mark
+                            state = ReadState::lookingForQuotation;
+                            break;
+                        }
+                        case ReadState::inQuotation: {
+                            length++;      // identified as a normal string
+                            break;
+                        }
+                        case ReadState::lookingForQuotation:
+                            return false;  // error;
+                    }
+                    break;
+                }
+
+                case '\n': {
+                    return false;     // error
+                }
+
+                default: {
+                    switch (state) {
+                        case ReadState::inQuotation: {
+                            length++;
+                            break;
+                        }
+                        case ReadState::lookingForComma:
+                        case ReadState::lookingForQuotation:
+                            break;   // just pass it
+                    }
+                }
+            }  // end: switch (ch)
+
+        } // end: traverse
+
+        return false;
+    }
+
+
+    bool CSVReader::readLine(std::istream &is, const std::vector<std::string *> &inputList) {
+
+        if(inputList.size() != m_cols) {
+            return false;
+        }
+
+        // get the next line to be parsed
+        std::string line;
+        std::getline(is, line);
+
+        // vectors of substring
+        std::vector<std::string_view> strings(m_cols);
+
+        // parse the line, looking for the separating commas
+        bool sign = traverse(line, strings, m_cols);
+
+        if(!sign) {
+            return false;
+        }
+
+        for(int i = 0; i < m_cols; ++i) {
+            *(inputList[i]) = strings[i];
+        }
+
+        return true;
+
+    }
+
+
 }
-
-
-
 
