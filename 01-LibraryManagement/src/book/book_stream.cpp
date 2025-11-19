872c53cbcd285b5984abdf6e3730eed561d026a3
NKanoya <3397540942@qq.com>
2025-11-20 00:07:04 +0800

    [LibraryManagement] (feat) implement `BookInfo` stream operators using CSV utils
    - updated the `BookInfo` input and output stream operators to utilize `Utils::CSVWriter` and `Utils::CSVReader` for handling CSV serialization and deserialization.

--- 01-LibraryManagement/src/book/book_stream.cpp
+++ 01-LibraryManagement/src/book/book_stream.cpp
@@ -3,15 +3,20 @@
 //
 
 #include "core/book.hpp"
+#include "Utils/csv.hpp"
 
-std::ostream& operator<<(std::ostream& os, const BookInfo& bookInfo) noexcept {
+std::ostream& operator<<(std::ostream& os, BookInfo& bookInfo) noexcept {
+    Utils::CSVWriter writer(bookInfo.KEYCOUNT);
 
+    writer.addEntry(os, bookInfo.getRange());
 }
 
 
 
-std::istream& operator>>(std::istream& os, BookInfo& bookInfo) noexcept {
+std::istream& operator>>(std::istream& is, BookInfo& bookInfo) noexcept {
+    Utils::CSVReader reader(bookInfo.KEYCOUNT);
 
+    reader.readLine(is, bookInfo.getRange());
 }
 
 
