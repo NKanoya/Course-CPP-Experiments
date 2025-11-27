583701faed720877f796ae5d5ac12803c75b4189
NKanoya <3397540942@qq.com>
2025-11-27 21:38:29 +0800

    [OnlineShoppingManagement] (feat) add `AmountOfMoney` class and integrate core module
    - introduce the `AmountOfMoney` class for precise monetary calculations, including arithmetic and formatting utilities
    - update CMake configuration to modularize the core library and link it with the main executable

---
+++ 02-OnlineShoppingManagement/include/core/amount_of_money.hpp
@@ -0,0 +1,103 @@
+//
+// Created on 2025/11/26.
+//
+
+#ifndef ONLINESHOPPINGMANAGEMENT_CORE_TYPES_HPP
+#define ONLINESHOPPINGMANAGEMENT_CORE_TYPES_HPP
+
+#include <cstdint>
+#include <string>
+
+class AmountOfMoney {
+    int64_t m_amount;
+public:
+    AmountOfMoney() : m_amount(0) {}
+    AmountOfMoney(int64_t amount) : m_amount(amount * 100) {}
+    AmountOfMoney(int32_t amount) : m_amount(amount * 100) {}
+    AmountOfMoney(int16_t amount) : m_amount(amount * 100) {}
+    AmountOfMoney(int8_t amount) : m_amount(amount * 100) {}
+    AmountOfMoney(uint32_t amount) : m_amount(amount * 100) {}
+    AmountOfMoney(uint16_t amount) : m_amount(amount * 100) {}
+    AmountOfMoney(uint8_t amount) : m_amount(amount * 100) {}
+
+    AmountOfMoney(double amount) : m_amount(amount * 100) {}
+    AmountOfMoney(const AmountOfMoney& oth) : m_amount(oth.m_amount) {}
+
+    double getDoubleAmount() {
+        return m_amount / 100.0;
+    }
+
+    inline AmountOfMoney operator+(const AmountOfMoney& oth) const {
+        return AmountOfMoney{m_amount + oth.m_amount};
+    }
+
+    inline AmountOfMoney operator-(const AmountOfMoney& oth) const {
+        return AmountOfMoney{m_amount - oth.m_amount};
+    }
+
+    inline AmountOfMoney operator*(int count) const {
+        return AmountOfMoney{m_amount * count};
+    }
+
+    friend inline AmountOfMoney operator*(int64_t count, const AmountOfMoney& oth);
+
+    inline AmountOfMoney operator+=(const AmountOfMoney& oth) {
+        m_amount += oth.m_amount;
+        return *this;
+    }
+
+    inline AmountOfMoney operator-=(const AmountOfMoney& oth) {
+        m_amount -= oth.m_amount;
+        return *this;
+    }
+
+    inline AmountOfMoney operator*=(int count) {
+        m_amount *= count;
+        return *this;
+    }
+
+    inline bool operator<(const AmountOfMoney& oth) const {
+        return m_amount < oth.m_amount;
+    }
+
+    inline bool operator>(const AmountOfMoney& oth) const {
+        return m_amount > oth.m_amount;
+    }
+
+    inline bool operator>=(const AmountOfMoney& oth) const {
+        return m_amount >= oth.m_amount;
+    }
+
+    inline bool operator<=(const AmountOfMoney& oth) const {
+        return m_amount <= oth.m_amount;
+    }
+
+    inline bool operator==(const AmountOfMoney& oth) const {
+        return m_amount == oth.m_amount;
+    }
+
+    inline bool operator!=(const AmountOfMoney& oth) const {
+        return m_amount == oth.m_amount;
+    }
+
+    inline AmountOfMoney operator-() const {
+        return AmountOfMoney{-m_amount};
+    }
+
+    inline AmountOfMoney discount(uint8_t discount) const {
+        return AmountOfMoney{m_amount * (100 - discount) / 80};
+    }
+
+    static AmountOfMoney convertFromString(const std::string& str);
+
+    std::string format() const;
+
+};
+
+using Price = AmountOfMoney;
+
+inline AmountOfMoney operator*(int64_t count, const AmountOfMoney& oth) {
+    return AmountOfMoney{oth.m_amount * count};
+}
+
+#endif //ONLINESHOPPINGMANAGEMENT_CORE_TYPES_HPP
---
+++ 02-OnlineShoppingManagement/src/core/amount_of_money/amount_of_money.cpp
@@ -0,0 +1,42 @@
+//
+// Created on 2025/11/26.
+//
+
+#include "core/amount_of_money.hpp"
+#include <string>
+
+AmountOfMoney AmountOfMoney::convertFromString(const std::string& str) {
+    AmountOfMoney amount{1};
+    if(str.empty())
+        return 0;
+
+    size_t dotPos = str.find('.');
+
+    if(dotPos == std::string::npos) {
+        return std::stoll(str) * 100;  // 无小数点，整数金额
+    }
+
+    std::string integerStr = str.substr(0, dotPos);
+    std::string decimalStr = str.substr(dotPos + 1);
+
+    amount.m_amount *= std::stoll(integerStr) * 100;
+
+    if(decimalStr.length() == 1) {
+        amount.m_amount += (decimalStr[0] - '0') * 10;
+    } else if(decimalStr.length() >= 2) {
+        amount.m_amount += (decimalStr[0] - '0') * 10 + (decimalStr[1] - '0');
+    }
+
+    return amount;
+}
+
+std::string AmountOfMoney::format() const {
+    std::string str = std::to_string(m_amount);
+
+    if (str.length() <= 2) {
+        str.insert(0, 3 - str.length(), '0');
+    }
+
+    str.insert(str.end() - 2, '.');
+    return str;
+}
