bf4d39355df17ebf57ed5c6b47d5140d42ea7825
NKanoya <3397540942@qq.com>
2025-11-27 21:45:24 +0800

    [OnlineShoppingManagement] (feat) include `rapidcsv` lib with CSV utility and integration
    - integrate the `rapidcsv` library for CSV parsing by adding its header and implementation files.
    - add a custom CSV converter for the Price type.

---
+++ 02-OnlineShoppingManagement/include/utils/csv/csv.hpp
@@ -0,0 +1,28 @@
+//
+// Created by pilip on 2025/11/26.
+//
+
+#ifndef ONLINE_SHOPPING_MANAGEMENT_CSV_HPP
+#define ONLINE_SHOPPING_MANAGEMENT_CSV_HPP
+
+#include "rapidcsv.h"
+#include "core/amount_of_money.hpp"
+
+namespace rapidcsv {
+
+    template<>
+    struct Converter<Price> {
+        Converter(const rapidcsv::ConverterParams& param) {}
+
+        void ToVal(const std::string& str, AmountOfMoney& val) {
+            val = Price::convertFromString(str);
+        }
+
+        static std::string ToStr(const Price& item, std::string str) {
+            str = item.format();
+        }
+    };
+
+}
+
+#endif //ONLINE_SHOPPING_MANAGEMENT_CSV_HPP
---
+++ 02-OnlineShoppingManagement/src/utils/rapidcsv/rapidcsv.cpp
@@ -0,0 +1,6 @@
+//
+// Created by pilip on 2025/11/26.
+//
+
+#include "utils/csv/csv.hpp"
+
